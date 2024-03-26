c3bcb1c491c4c474319440c252af6eaa07364fd3
Kitichkoo <148893669+Foskys@users.noreply.github.com>
2024-03-26 23:05:33 +0300

    Add files via upload

---
+++ laba3var4/lab3-4-1/lab3-4-1.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+int main() {
+    setlocale(LC_CTYPE, "Rus");
+    ifstream file1("FILE1.txt");
+    if (!file1.is_open()) {
+        cerr << "Ошибка открытия файла FILE1.txt\n";
+        return 1;
+    }
+
+    ofstream file2("FILE2.txt");
+    if (!file2.is_open()) {
+        cerr << "Ошибка открытия файла FILE2.txt\n";
+        return 1;
+    }
+
+    int countLinesWithA = 0;
+    string line;
+    while (getline(file1, line)) {
+        bool containsDigit = false;
+        for (char c : line) {
+            if (isdigit(c)) {
+                containsDigit = true;
+                break;
+            }
+        }
+
+        if (!containsDigit) {
+            file2 << line << endl;
+            if (!line.empty() && line[0] == 'A') {
+                countLinesWithA++;
+            }
+        }
+    }
+
+    file1.close();
+    file2.close();
+
+    cout << "Количество строк, начинающихся на букву 'A' в файле FILE2: " << countLinesWithA << endl;
+
+    return 0;
+}
---
+++ laba3var4/lab3-4-2/lab3-4-2.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+using namespace std;
+
+int main() {
+    setlocale(LC_CTYPE, "Rus");
+    string inputString;
+    cout << "Введите строку, состоящую из цифр и слов, разделенных пробелами: ";
+    getline(cin, inputString);
+
+    ofstream file("input.txt");
+    if (!file.is_open()) {
+        cerr << "Ошибка открытия файла для записи\n";
+        return 1;
+    }
+
+    file << inputString;
+    file.close();
+
+    ifstream inputFile("input.txt");
+    if (!inputFile.is_open()) {
+        cerr << "Ошибка открытия файла для чтения\n";
+        return 1;
+    }
+
+    string word;
+    int maxWordLength = 0;
+    while (inputFile >> word) {
+        int wordLength = word.length();
+        if (wordLength > maxWordLength) {
+            maxWordLength = wordLength;
+        }
+    }
+
+    inputFile.close();
+
+    cout << "Самое длинное слово содержит " << maxWordLength << " символов.\n";
+
+    return 0;
+}
