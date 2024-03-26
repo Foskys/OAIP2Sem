f315027c2d22e93eff2cb27e68f40acead917b02
Kitichkoo <148893669+Foskys@users.noreply.github.com>
2024-03-26 23:02:37 +0300

    Add files via upload

---
+++ laba3var7/lab3-7-1/lab3-7-1.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+int main() {
+    //потоки для чтения/записи
+    ifstream in("in.txt");
+    ofstream out("out.txt");
+    string line;
+    unsigned int amount = 0;
+    bool exist = false;
+    //пока есть строки в файле
+    while (getline(in, line)) {
+        for (unsigned int i = 0; i < line.length(); i++)
+            if (isdigit((unsigned char)line[i]))
+                exist = true;
+
+        if (!exist) {
+            out << line << '\n';
+            if (line[0] == 'A' || line[0] == 'a') //проверяем начинается ли строка с буквы А
+                amount++;
+        }
+        else
+            exist = false;
+    }
+    out << '\n' << amount; //записываем количество строк начинающихся с буквы А
+}
---
+++ laba3var7/lab3-7-2/lab3-7-2.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+void read(ifstream& f, string str) {
+    getline(f, str, '\n');
+}
+void write(ofstream& f, string str) {
+    f << str;
+}
+int main() {
+    //потоки для чтения, записи
+    ifstream in("in.txt");
+    ofstream out("out.txt");
+    char tmp[100] = " "; //буфер для записи самого длинного слова
+    string line, prikol;
+    getline(cin, line, '\n'); //получаем из потока консоли набранный пользователем текст
+    write(out, line); //записываем текст в файл
+    out.close(); //закрываем поток для записи
+    read(in, prikol); //читаем строку из файла
+    char* split = _strdup(prikol.c_str());  //преобразуем string строку в char[] для возможности использовать strtok()
+    char* token = strtok(split, " "); //разделяем строку на слова
+    while (token != NULL) {
+        if (strlen(token) > strlen(tmp)) strcpy(tmp, token); //если слово превосходит самое длинное последнее слово то оно становится самым длинным
+        token = strtok(NULL, " "); //переход к следующему слову
+    }
+    cout << "The longest word is: " << tmp << " and its length is " << strlen(tmp); //вывод результата
+}
