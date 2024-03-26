742b76708db494aa60661fe0397ea8b1d68afe0d
Kitichkoo <148893669+Foskys@users.noreply.github.com>
2024-03-26 23:01:19 +0300

    Add files via upload

---
+++ laba3var2/lab3-2-1/lab3-2-1.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+int main() {
+    ofstream fout;
+    ifstream fin;
+    string file1 = "file1.txt", file2 = "file2.txt";
+    fin.open(file1);
+    string str[20];
+    int count = 0;
+    //считаем количество строк в первом файле
+    while (!fin.eof()) {
+        getline(fin, str[count]);
+        count++;
+    }
+    fin.close();
+    fout.open(file2);
+    string newStr[50];
+    int size = 0;
+    //копируем строки начинающиеся на А или а
+    for (int i = 0; i < count; i++) {
+        if (str[i][0] == 'A' || str[i][0] == 'a') {
+            fout << str[i] << endl;
+            newStr[size] = str[i];
+            size++;
+        }
+    }
+    //подсчет слов во втором файле
+    int count_word = 0;
+    for (int i = 0; i < size; i++) {
+        newStr[i] += " ";
+        for (int j = 0; j < newStr[i].length(); j++) {
+            if (newStr[i][j] == ' ' && newStr[i][j - 1] != ' ') {
+                count_word++;
+            }
+        }
+    }
+    cout << count_word << endl;
+    fout.close();
+    return 0;
+}
---
+++ laba3var2/lab3-2-2/lab3-2-2.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+//функции чтения/записи
+void read(ifstream& f, string str) {
+	getline(f, str);
+}
+void write(ofstream& f, string str) {
+	f << str;
+}
+int main() {
+	ofstream fout("fout.txt");
+	ifstream fin("fin.txt");
+	string str;
+	//вводим в консоль, записываем и читаем
+	getline(cin, str);
+	write(fout, str);
+	read(fin, str);
+	int count = 0, arr[50], size = 0, index = 0;
+	str += " ";
+	//подсчет размера каждой группы
+	for (int i = 0; i < str.length(); i++) {
+		if (str[i] != ' ') count++;
+		else {
+			arr[size] = count;
+			size++;
+			count = 0;
+		}
+	}
+	//поиск самой маленькой группы
+	int min = arr[0];
+	for (int i = 0; i < size; i++) {
+		if (arr[i] < min) {
+			min = arr[i];
+			index = i;
+		}
+	}
+	string group;
+	for (int i = 0; i < str.length(); i++) {
+		if (str[i] == ' ' && str[i - 1] != ' ') count++;
+		else if (count == index) group += str[i];
+	}
+	//выводим самую маленькую группу
+	cout << group << endl;
+	return 0;
+}
