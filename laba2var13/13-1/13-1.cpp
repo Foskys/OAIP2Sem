b4a2165bc0f38e12e6f643853e70b4675684a041
Kitichkoo <148893669+Foskys@users.noreply.github.com>
2024-03-26 22:56:30 +0300

    Add files via upload

---
+++ laba2var13/13-1/13-1.cpp
@@ -0,0 +1,68 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+
+#define MAX_SIZE 1000 // Максимальное количество чисел в файле fileA
+int main() {
+    setlocale(LC_CTYPE, "rus");
+    FILE* fileA, * fileB;
+    int numbers[MAX_SIZE];
+    int count[MAX_SIZE] = { 0 };
+    int size = 0;
+
+    // Открытие файла для чтения
+    fileA = fopen("fA.txt", "r");
+    if (fileA == NULL) {
+        printf("Ошибка при открытии файла fA.txt.\n");
+        return 1;
+    }
+
+    // Считывание чисел из файла fileA
+    while (fscanf_s(fileA, "%d", &numbers[size]) == 1) {
+        size++;
+    }
+
+    // Подсчет количества повторений каждого числа
+    for (int i = 0; i < size; i++) {
+        int current = numbers[i];
+        int found = 0;
+
+        for (int j = 0; j < i; j++) {
+            if (numbers[j] == current) {
+                found = 1;
+                break;
+            }
+        }
+
+        if (!found) {
+            for (int j = i; j < size; j++) {
+                if (numbers[j] == current) {
+                    count[i]++;
+                }
+            }
+        }
+    }
+
+    // Открытие файла для записи
+    fileB = fopen("fB.txt", "w");
+    if (fileB == NULL) {
+        printf("Ошибка при открытии файла fB.txt.\n");
+        return 1;
+    }
+
+    // Запись чисел, встречающихся более двух раз, в файл fileB
+    for (int i = 0; i < size; i++) {
+        if (count[i] > 2) { // Изменено условие для чисел, встречающихся более двух раз
+            fprintf(fileB, "%d\n", numbers[i]);
+        }
+    }
+
+    // Закрытие файлов
+    fclose(fileA);
+    fclose(fileB);
+
+    printf("Программа успешно завершила работу. Результат записан в файл fB.txt\n");
+
+    return 0;
+}
---
+++ laba2var13/13-2/13-2.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <fstream>
+
+int main() {
+    setlocale(LC_CTYPE, "rus");
+    std::ifstream fileF("F1.txt");
+    std::ofstream fileG("F2.txt");
+
+    int K;
+    std::cout << "Введите число K: ";
+    std::cin >> K;
+
+    int num;
+    while (fileF >> num) {
+        if (num % K == 0) {
+            fileG << num << std::endl;
+        }
+    }
+
+    fileF.close();
+    fileG.close();
+
+    return 0;
+}
