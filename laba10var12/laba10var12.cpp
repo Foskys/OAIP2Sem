53053af5eb883505ffaeddfd835632a905532cae
Kitichkoo <148893669+Foskys@users.noreply.github.com>
2024-04-28 01:02:35 +0300

    Add files via upload

---
+++ laba10var12/laba10var12.cpp
@@ -0,0 +1,22 @@
+﻿
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+// Рекурсивная функция для вычисления суммы косинусов
+double sumCosines(int n, double x) {
+    // Базовый случай: когда n = 1, просто возвращаем cos(x)
+    if (n == 1)
+        return cos(x);
+    // Рекурсивный случай: суммируем cos(nx) с результатом для n-1
+    return cos(n * x) + sumCosines(n - 1, x);
+}
+
+int main() {
+    setlocale(LC_ALL, "Rus");
+    double x = 0.5; // Значение x
+    int n = 5; // Количество слагаемых
+    double result = sumCosines(n, x);
+    cout << "Сумма косинусов для x=" << x << " и n=" << n << " равна " << result << endl;
+    return 0;
+}
