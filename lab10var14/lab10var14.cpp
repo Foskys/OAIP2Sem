505df53469a06d5cef494c91a1bd8aa00450eaa8
Kitichkoo <148893669+Foskys@users.noreply.github.com>
2024-04-28 00:52:21 +0300

    Add files via upload

---
+++ lab10var14/lab10var14.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+using namespace std;
+
+double f(double x, int n) {
+    if (n == 0) {
+        return 1;
+    }
+    else if (n == 1) {
+        return x;
+    }
+    else {
+        return (x * x / n / (n - 1)) * f(x, n - 2);
+    }
+}
+
+int main() {
+    setlocale(LC_ALL,"rus");
+    double x;
+    int n;
+
+    cout << "Введите значение x: ";
+    cin >> x;
+    cout << "Введите значение n: ";
+    cin >> n;
+
+    double result = f(x, n);
+    cout << "Результат вычисления: " << result << endl;
+
+    return 0;
+}
